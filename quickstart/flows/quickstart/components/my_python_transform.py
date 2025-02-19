d50e1231c7c58aa5bc162e476c6e2a43b36c81a7
Cody Peterson <54814569+lostmygithubaccount@users.noreply.github.com>
2025-02-19 10:01:20 -0500

    remove old quickstart/ dir (#14)
    subsumed by `quickstarts/` and (I believe at this point) out of date and
    no longer needed w/ the alternative `quickstarts/` code

--- quickstart/flows/quickstart/components/my_python_transform.py
+++
@@ -1,9 +0,0 @@
-from ibis import ir
-
-from ascend.resources import ref, transform
-
-
-@transform(inputs=[ref("lake_reader")])
-def my_python_transform(lake_reader: ir.Table, context) -> ir.Table:
-  # lake_reader is an Ibis Table object containing records from the lake_reader component
-  return lake_reader
