f94803a3618c11022418e926a16191e220f9ed23
Cody Peterson <54814569+lostmygithubaccount@users.noreply.github.com>
2025-03-04 14:24:30 -0800

    add ottos-expedition/projects/snowflake (#16)
    Based off the Otto's Expeditions project for BigQuery, add a project for
    Snowflake. Changes include:
    
    - uppercase identifiers throughout
    - remove write connectors (not supported yet)
        - adjust the alias in analytics flow accordingly
        
    The `elt` Flow has 53 components, and the `analytics` Flow has 5. the
    `elt` Flow looks like:
    
    
    ![image](https://github.com/user-attachments/assets/e9db417f-8c25-467d-9fae-70318825130c)
    
    TODOs (in this PR or follow up):
    
    - [x] add Snowpark components

--- ottos-expeditions/dev.py
+++ ottos-expeditions/dev.py
@@ -1,3 +1,11 @@
+"""
+This file is used for local development and testing. Typically, in IPython:
+
+```python
+$ ipython -i dev.py
+```
+"""
+
 # ruff: noqa
 # imports
 import os
@@ -9,6 +17,10 @@ import ibis.selectors as s
 from io import BytesIO, StringIO
 from faker import Faker
 from dotenv import load_dotenv
+
+from snowflake.snowpark import Session
+from snowflake.snowpark.types import StringType
+from snowflake.snowpark.functions import lit
 
 from ottos_expeditions.lib.synthetic import *
 
@@ -46,12 +58,15 @@ bq_con = ibis.bigquery.connect(project_id=project_id, dataset_id=dataset_id)
 snow_dev_profile = yaml.safe_load(
     open(os.path.join("projects", "snowflake", "profiles", "dev.yaml"))
 )
-snow_con = ibis.snowflake.connect(
-    account=snow_dev_profile["profile"]["parameters"]["snowflake"]["account"],
-    user=snow_dev_profile["profile"]["parameters"]["snowflake"]["user"],
-    database=snow_dev_profile["profile"]["parameters"]["snowflake"]["database"],
-    schema=snow_dev_profile["profile"]["parameters"]["snowflake"]["schema"],
-    warehouse=snow_dev_profile["profile"]["parameters"]["snowflake"]["warehouse"],
-    role=snow_dev_profile["profile"]["parameters"]["snowflake"]["role"],
-    password=os.getenv("SNOWFLAKE_PASSWORD"),
-)
+
+snow_params = {
+    "account": snow_dev_profile["profile"]["parameters"]["snowflake"]["account"],
+    "user": snow_dev_profile["profile"]["parameters"]["snowflake"]["user"],
+    "password": os.getenv("SNOWFLAKE_PASSWORD"),
+    "role": snow_dev_profile["profile"]["parameters"]["snowflake"]["role"],
+    "warehouse": snow_dev_profile["profile"]["parameters"]["snowflake"]["warehouse"],
+    "database": snow_dev_profile["profile"]["parameters"]["snowflake"]["database"],
+    "schema": snow_dev_profile["profile"]["parameters"]["snowflake"]["schema"],
+}
+snow_con = ibis.snowflake.connect(**snow_params)
+snow_ses = Session.builder.configs(snow_params).create()
